30182b4106f5f136bd7f5ffe50057a034bbd7902
Juan Albornoz <jualbornozc@gmail.com>
2023-07-23 23:47:24 -0400

    Se trabaja en el admin.py para las configuraciones del sitio administrativo

--- laboratorio/admin.py
+++ laboratorio/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Laboratorio, DirectorGeneral, Producto
 
 # Register your models here.
+
+class LaboratorioAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre')
+    ordering = ['created', 'id']
+
+class DirectorGeneralAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre', 'laboratorio')
+    ordering = ['created', 'nombre']
+
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre', 'laboratorio', 'f_fabricacion', 'p_costo', 'p_venta')
+    ordering = ['created', 'nombre', 'laboratorio']
+    list_filter = ('nombre', 'laboratorio')
+
+admin.site.register(Laboratorio, LaboratorioAdmin)
+admin.site.register(DirectorGeneral, DirectorGeneralAdmin)
+admin.site.register(Producto, ProductoAdmin)
--- laboratorio/models.py
+++ laboratorio/models.py
@@ -19,7 +19,7 @@ class Laboratorio(models.Model):
     
 class DirectorGeneral(models.Model):
     nombre = models.CharField(max_length=250, verbose_name="Nombre Director")
-    laboratorio = models.OneToOneField("Laboratorio", on_delete=models.CASCADE)
+    laboratorio = models.OneToOneField("Laboratorio", on_delete=models.CASCADE, verbose_name="Laboratorio")
     created = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de Creación')
     updated = models.DateTimeField(auto_now=True, verbose_name='Fecha de actualización')
 
@@ -28,8 +28,8 @@ class DirectorGeneral(models.Model):
     
 class Producto(models.Model):
     nombre = models.CharField(max_length=250, verbose_name="Nombre Producto")
-    laboratorio = models.ForeignKey("Laboratorio", on_delete=models.CASCADE)
-    f_fabricacion = models.DateField(validators=[], default=datetime.date.today, verbose_name="Fecha de Fabricacion")
+    laboratorio = models.ForeignKey("Laboratorio", on_delete=models.CASCADE, verbose_name="Laboratorio")
+    f_fabricacion = models.IntegerField(choices=annos_choices, verbose_name="Fecha de Fabricacion")
     p_costo = models.DecimalField(decimal_places=2, max_digits=12, verbose_name="Precio Costo")
     p_venta = models.DecimalField(decimal_places=2, max_digits=12, verbose_name="Precio Venta")
     created = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de Creación')
